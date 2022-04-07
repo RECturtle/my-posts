20f0d6183b50518f0a749112958be16da780a46f
RECturtle <spencer.mertes@gmail.com>
2022-04-07 19:55:34 -0400

    add some functionality and styling

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+end
